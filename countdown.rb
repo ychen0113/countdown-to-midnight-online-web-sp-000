9bd52d7f546667dc7240fde6720a77519d167b07
Yiliang Chen <yiliangchen0113@gmail.com>
2020-09-27 20:14:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0 do 
+        number =- 1
+    puts "#{number} SECOND(S)!"
+
+  end
+  
+  if number == 0 
+    puts "HAPPY NEW YEAR!"
+  end 
 end
